410ae7efe9e238cada0b741e2a29c152cebb0dd0
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-02-01 06:53:03 +0100

    config

--- src/readme.rs
+++ src/readme.rs
@@ -1,44 +1,32 @@
 use serde::{Serialize, Deserialize};
 
 const EMPTY: &str = "";
-const FALSE: bool = false;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Readme {
+    // user of GitHub/GitLab
     #[serde(default = "get_empty")]
-    pub name: String,
-    #[serde(default = "get_empty")]
-    pub title: String,
+    pub user: String,
+    // project of GitHub/GitLab
+    // user/project
     #[serde(default = "get_empty")]
     pub project: String,
+    #[serde(default = "get_empty")]
+    pub title: String,
     #[serde(default = "get_empty")]
     pub license: String,
     #[serde(default = "get_empty")]
     pub icon: String,
     #[serde(default = "get_empty")]
     pub homepage: String,
-    #[serde(default = "get_false")]
-    pub license_badge: bool,
-    #[serde(default = "get_false")]
-    pub contributors_badge: bool,
-    #[serde(default = "get_false")]
-    pub lastcommit_badge: bool,
-    #[serde(default = "get_false")]
-    pub codefactor_badge: bool,
 }
 
 fn get_empty() -> String{
     return EMPTY.to_string();
 }
 
-fn get_false() -> bool{
-    return FALSE;
-}
-
 impl Readme {
-    // add code here
     pub fn from_file(file: &str) -> Self{
-
 
     }
 }
